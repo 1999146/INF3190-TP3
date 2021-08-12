8ee63650216d9137caabe22c925273e7351dea93
alex laurin <alexlaurincoding@gmail.com>
2021-08-11 21:19:03 -0400

    commit bidon

---
+++ src/app/bidon/bidon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bidon',
+  templateUrl: './bidon.component.html',
+  styleUrls: ['./bidon.component.scss']
+})
+export class BidonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
