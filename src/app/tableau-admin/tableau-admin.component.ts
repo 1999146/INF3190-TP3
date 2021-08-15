ae0756d947910697afbb833b1fbbb44d827a4777
alex laurin <alexlaurincoding@gmail.com>
2021-08-14 23:14:55 -0400

    affichage du tableau admin fonctionnel 2

--- src/app/tableau-admin/tableau-admin.component.ts
+++ src/app/tableau-admin/tableau-admin.component.ts
@@ -7,6 +7,7 @@ import {IEnfant, IInscriptionParent, IParent} from "../classes/interface-json/in
 import {Enfant, Inscription, InscriptionParent} from "../classes/parent";
 import {IGabaritProgramme, IProgramme, ISession} from "../classes/interface-json/interface-session";
 import {applySourceSpanToExpressionIfNeeded} from "@angular/compiler/src/output/output_ast";
+import {Session} from "../classes/session";
 
 @Component({
   selector: 'app-tableau-admin',
@@ -30,10 +31,7 @@ export class TableauAdminComponent implements OnInit {
       this.router.navigate(['']);
     }else{
       this.inscriptionsParents = authService.inscriptionsParents;
-      console.log(this.inscriptionsParents);
       this.inscriptions = Join.getInscriptions(this.inscriptionsParents);
-      console.log(this.inscriptions);
-      console.log(this.parents)
       this.sessions = this.authService.sessions;
       this.programmes = this.authService.gabaritProgrammes;
 
@@ -67,9 +65,17 @@ export class TableauAdminComponent implements OnInit {
   }
 
   getNomProgramme(idProgramme: string){
-    let codeProgramme = idProgramme.slice(0,2);
+    let codeProgramme = "G" + idProgramme.charAt(1);
    console.log(codeProgramme);
     return Join.getGabaritProgrammeById(this.programmes, codeProgramme)?.titre;
+  }
+
+  getNomProgrammeModal(idProgramme: string | undefined){
+    if(idProgramme !== null && idProgramme !== undefined){
+      let codeProgramme = "G" + idProgramme.charAt(1);
+      return Join.getGabaritProgrammeById(this.programmes, codeProgramme)?.titre;
+    }
+    return "";
   }
 
   getParentById(idParent: string){
