57e87c06516784d0a1c1c23effcbf169b58107d6
comtois-etienne <84880788+comtois-etienne@users.noreply.github.com>
2021-08-21 01:21:05 -0400

    json sessions

--- src/data/sessions.ts
+++ src/data/sessions.ts
@@ -1,434 +1,435 @@
-export default [
-  {
-    semaines: [
-      {
-        idProgrammes: [
-         'P0_S22',
-         'P1_S22',
-         'P2_S22',
-        ],
-        id: 'S1_S22',
-        noSemaine: 1,
-      },
-      {
-        idProgrammes: [
-         'P0_S22',
-         'P1_S22',
-         'P2_S22',
-        ],
-        id: 'S2_S22',
-        noSemaine: 2,
-      },
-      {
-        idProgrammes: [
-         'P0_S22',
-         'P1_S22',
-         'P2_S22',
-        ],
-        id: 'S3_S22',
-        noSemaine: 3,
-      },
-      {
-        idProgrammes: [
-         'P0_S22',
-         'P1_S22',
-         'P2_S22',
-        ],
-        id: 'S4_S22',
-        noSemaine: 4,
-      },
-      {
-        idProgrammes: [
-         'P0_S22',
-         'P1_S22',
-         'P2_S22',
-        ],
-        id: 'S5_S22',
-        noSemaine: 5,
-      },
-      {
-        idProgrammes: [
-         'P0_S22',
-         'P1_S22',
-         'P2_S22',
-        ],
-        id: 'S6_S22',
-        noSemaine: 6,
-      },
-      {
-        idProgrammes: [
-         'P0_S22',
-         'P1_S22',
-         'P2_S22',
-        ],
-        id: 'S7_S22',
-        noSemaine: 7,
-      },
-      {
-        idProgrammes: [
-         'P0_S22',
-         'P1_S22',
-         'P2_S22',
-        ],
-        id: 'S8_S22',
-        noSemaine: 8,
-      },
-      {
-        idProgrammes: [
-         'P0_S22',
-         'P1_S22',
-         'P2_S22',
-        ],
-        id: 'S9_S22',
-        noSemaine: 9,
-      },
-      {
-        idProgrammes: [
-         'P0_S22',
-         'P1_S22',
-         'P2_S22',
-        ],
-        id: 'S10_S22',
-        noSemaine: 10,
-      },
-      {
-        idProgrammes: [
-         'P0_S22',
-         'P1_S22',
-         'P2_S22',
-        ],
-        id: 'S11_S22',
-        noSemaine: 11,
-      },
-      {
-        idProgrammes: [
-         'P0_S22',
-         'P1_S22',
-         'P2_S22',
-        ],
-        id: 'S12_S22',
-        noSemaine: 12,
-      },
-      {
-        idProgrammes: [
-         'P0_S22',
-         'P1_S22',
-         'P2_S22',
-        ],
-        id: 'S13_S22',
-        noSemaine: 13,
-      },
-      {
-        idProgrammes: [
-         'P0_S22',
-         'P1_S22',
-         'P2_S22',
-        ],
-        id: 'S14_S22',
-        noSemaine: 14,
-      },
-      {
-        idProgrammes: [
-         'P0_S22',
-         'P1_S22',
-         'P2_S22',
-        ],
-        id: 'S15_S22',
-        noSemaine: 15,
-      },
-    ],
-    id: 'S22',
-    nom: 'Session 2022',
-    description: 'Session plein de plaisir',
-    dateDebut: '2022-04-01',
-    dateFin: '2022-08-01',
-  },
-  {
-   semaines: [
-     {
-       idProgrammes: [
-        'P0_S20',
-        'P1_S20',
-        'P2_S20',
-       ],
-       id: 'S1_S20',
-       noSemaine: 1,
-     },
-     {
-       idProgrammes: [
-        'P0_S20',
-        'P1_S20',
-        'P2_S20',
-       ],
-       id: 'S2_S20',
-       noSemaine: 2,
-     },
-     {
-       idProgrammes: [
-        'P0_S20',
-        'P1_S20',
-        'P2_S20',
-       ],
-       id: 'S3_S20',
-       noSemaine: 3,
-     },
-     {
-       idProgrammes: [
-        'P0_S20',
-        'P1_S20',
-        'P2_S20',
-       ],
-       id: 'S4_S20',
-       noSemaine: 4,
-     },
-     {
-       idProgrammes: [
-        'P0_S20',
-        'P1_S20',
-        'P2_S20',
-       ],
-       id: 'S5_S20',
-       noSemaine: 5,
-     },
-     {
-       idProgrammes: [
-        'P0_S20',
-        'P1_S20',
-        'P2_S20',
-       ],
-       id: 'S6_S20',
-       noSemaine: 6,
-     },
-     {
-       idProgrammes: [
-        'P0_S20',
-        'P1_S20',
-        'P2_S20',
-       ],
-       id: 'S7_S20',
-       noSemaine: 7,
-     },
-     {
-       idProgrammes: [
-        'P0_S20',
-        'P1_S20',
-        'P2_S20',
-       ],
-       id: 'S8_S20',
-       noSemaine: 8,
-     },
-     {
-       idProgrammes: [
-        'P0_S20',
-        'P1_S20',
-        'P2_S20',
-       ],
-       id: 'S9_S20',
-       noSemaine: 9,
-     },
-     {
-       idProgrammes: [
-        'P0_S20',
-        'P1_S20',
-        'P2_S20',
-       ],
-       id: 'S10_S20',
-       noSemaine: 10,
-     },
-     {
-       idProgrammes: [
-        'P0_S20',
-        'P1_S20',
-        'P2_S20',
-       ],
-       id: 'S11_S20',
-       noSemaine: 11,
-     },
-     {
-       idProgrammes: [
-        'P0_S20',
-        'P1_S20',
-        'P2_S20',
-       ],
-       id: 'S12_S20',
-       noSemaine: 12,
-     },
-     {
-       idProgrammes: [
-        'P0_S20',
-        'P1_S20',
-        'P2_S20',
-       ],
-       id: 'S13_S20',
-       noSemaine: 13,
-     },
-     {
-       idProgrammes: [
-        'P0_S20',
-        'P1_S20',
-        'P2_S20',
-       ],
-       id: 'S14_S20',
-       noSemaine: 14,
-     },
-     {
-       idProgrammes: [
-        'P0_S20',
-        'P1_S20',
-        'P2_S20',
-       ],
-       id: 'S15_S20',
-       noSemaine: 15,
-     },
-   ],
-   id: 'S20',
-   nom: 'Session 2020',
-   description: 'Session plein de plaisir',
-   dateDebut: '2020-04-01',
-   dateFin: '2020-08-01',
- },
- {
-   semaines: [
-     {
-       idProgrammes: [
-        'P0_S21',
-        'P1_S21',
-        'P2_S21',
-       ],
-       id: 'S1_S21',
-       noSemaine: 1,
-     },
-     {
-        idProgrammes: [
-        'P0_S21',
-        'P1_S21',
-        'P2_S21',
-       ],
-       id: 'S2_S21',
-       noSemaine: 2,
-     },
-     {
-        idProgrammes: [
-        'P0_S21',
-        'P1_S21',
-        'P2_S21',
-       ],
-       id: 'S3_S21',
-       noSemaine: 3,
-     },
-     {
-        idProgrammes: [
-        'P0_S21',
-        'P1_S21',
-        'P2_S21',
-       ],
-       id: 'S4_S21',
-       noSemaine: 4,
-     },
-     {
-        idProgrammes: [
-        'P0_S21',
-        'P1_S21',
-        'P2_S21',
-       ],
-       id: 'S5_S21',
-       noSemaine: 5,
-     },
-     {
-       idProgrammes: [
-        'P0_S21',
-        'P1_S21',
-        'P2_S21',
-       ],
-       id: 'S6_S21',
-       noSemaine: 6,
-     },
-     {
-       idProgrammes: [
-        'P0_S21',
-        'P1_S21',
-        'P2_S21',
-       ],
-       id: 'S7_S21',
-       noSemaine: 7,
-     },
-     {
-       idProgrammes: [
-        'P0_S21',
-        'P1_S21',
-        'P2_S21',
-       ],
-       id: 'S8_S21',
-       noSemaine: 8,
-     },
-     {
-       idProgrammes: [
-        'P0_S21',
-        'P1_S21',
-        'P2_S21',
-       ],
-       id: 'S9_S21',
-       noSemaine: 9,
-     },
-     {
-       idProgrammes: [
-        'P0_S21',
-        'P1_S21',
-        'P2_S21',
-       ],
-       id: 'S10_S21',
-       noSemaine: 10,
-     },
-     {
-       idProgrammes: [
-        'P0_S21',
-        'P1_S21',
-        'P2_S21',
-       ],
-       id: 'S11_S21',
-       noSemaine: 11,
-     },
-     {
-       idProgrammes: [
-        'P0_S21',
-        'P1_S21',
-        'P2_S21',
-       ],
-       id: 'S12_S21',
-       noSemaine: 12,
-     },
-     {
-       idProgrammes: [
-        'P0_S21',
-        'P1_S21',
-        'P2_S21',
-       ],
-       id: 'S13_S21',
-       noSemaine: 13,
-     },
-     {
-       idProgrammes: [
-        'P0_S21',
-        'P1_S21',
-        'P2_S21',
-       ],
-       id: 'S14_S21',
-       noSemaine: 14,
-     },
-     {
-       idProgrammes: [
-        'P0_S21',
-        'P1_S21',
-        'P2_S21',
-       ],
-       id: 'S15_S21',
-       noSemaine: 15,
-     },
-   ],
-   id: 'S21',
-   nom: 'Session 2021',
-   description: 'Session plein de plaisir',
-   dateDebut: '2021-07-18',
-   dateFin: '2021-12-18',
- },
-];
+export default
+[
+   {
+      "semaines": [
+         {
+            "idProgrammes": [
+               "P0_S21",
+               "P1_S21",
+               "P2_S21"
+            ],
+            "id": "S1_S21",
+            "noSemaine": 1
+         },
+         {
+            "idProgrammes": [
+               "P0_S21",
+               "P1_S21",
+               "P2_S21"
+            ],
+            "id": "S2_S21",
+            "noSemaine": 2
+         },
+         {
+            "idProgrammes": [
+               "P0_S21",
+               "P1_S21",
+               "P2_S21"
+            ],
+            "id": "S3_S21",
+            "noSemaine": 3
+         },
+         {
+            "idProgrammes": [
+               "P0_S21",
+               "P1_S21",
+               "P2_S21"
+            ],
+            "id": "S4_S21",
+            "noSemaine": 4
+         },
+         {
+            "idProgrammes": [
+               "P0_S21",
+               "P1_S21",
+               "P2_S21"
+            ],
+            "id": "S5_S21",
+            "noSemaine": 5
+         },
+         {
+            "idProgrammes": [
+               "P0_S21",
+               "P1_S21",
+               "P2_S21"
+            ],
+            "id": "S6_S21",
+            "noSemaine": 6
+         },
+         {
+            "idProgrammes": [
+               "P0_S21",
+               "P1_S21",
+               "P2_S21"
+            ],
+            "id": "S7_S21",
+            "noSemaine": 7
+         },
+         {
+            "idProgrammes": [
+               "P0_S21",
+               "P1_S21",
+               "P2_S21"
+            ],
+            "id": "S8_S21",
+            "noSemaine": 8
+         },
+         {
+            "idProgrammes": [
+               "P0_S21",
+               "P1_S21",
+               "P2_S21"
+            ],
+            "id": "S9_S21",
+            "noSemaine": 9
+         },
+         {
+            "idProgrammes": [
+               "P0_S21",
+               "P1_S21",
+               "P2_S21"
+            ],
+            "id": "S10_S21",
+            "noSemaine": 10
+         },
+         {
+            "idProgrammes": [
+               "P0_S21",
+               "P1_S21",
+               "P2_S21"
+            ],
+            "id": "S11_S21",
+            "noSemaine": 11
+         },
+         {
+            "idProgrammes": [
+               "P0_S21",
+               "P1_S21",
+               "P2_S21"
+            ],
+            "id": "S12_S21",
+            "noSemaine": 12
+         },
+         {
+            "idProgrammes": [
+               "P0_S21",
+               "P1_S21",
+               "P2_S21"
+            ],
+            "id": "S13_S21",
+            "noSemaine": 13
+         },
+         {
+            "idProgrammes": [
+               "P0_S21",
+               "P1_S21",
+               "P2_S21"
+            ],
+            "id": "S14_S21",
+            "noSemaine": 14
+         },
+         {
+            "idProgrammes": [
+               "P0_S21",
+               "P1_S21",
+               "P2_S21"
+            ],
+            "id": "S15_S21",
+            "noSemaine": 15
+         }
+      ],
+      "id": "S21",
+      "nom": "Session 2021",
+      "description": "Session plein de plaisir",
+      "dateDebut": "2021-07-18",
+      "dateFin": "2021-12-18"
+   },
+   {
+      "semaines": [
+         {
+            "idProgrammes": [
+               "P0_S22",
+               "P1_S22",
+               "P2_S22"
+            ],
+            "id": "S1_S22",
+            "noSemaine": 1
+         },
+         {
+            "idProgrammes": [
+               "P0_S22",
+               "P1_S22",
+               "P2_S22"
+            ],
+            "id": "S2_S22",
+            "noSemaine": 2
+         },
+         {
+            "idProgrammes": [
+               "P0_S22",
+               "P1_S22",
+               "P2_S22"
+            ],
+            "id": "S3_S22",
+            "noSemaine": 3
+         },
+         {
+            "idProgrammes": [
+               "P0_S22",
+               "P1_S22",
+               "P2_S22"
+            ],
+            "id": "S4_S22",
+            "noSemaine": 4
+         },
+         {
+            "idProgrammes": [
+               "P0_S22",
+               "P1_S22",
+               "P2_S22"
+            ],
+            "id": "S5_S22",
+            "noSemaine": 5
+         },
+         {
+            "idProgrammes": [
+               "P0_S22",
+               "P1_S22",
+               "P2_S22"
+            ],
+            "id": "S6_S22",
+            "noSemaine": 6
+         },
+         {
+            "idProgrammes": [
+               "P0_S22",
+               "P1_S22",
+               "P2_S22"
+            ],
+            "id": "S7_S22",
+            "noSemaine": 7
+         },
+         {
+            "idProgrammes": [
+               "P0_S22",
+               "P1_S22",
+               "P2_S22"
+            ],
+            "id": "S8_S22",
+            "noSemaine": 8
+         },
+         {
+            "idProgrammes": [
+               "P0_S22",
+               "P1_S22",
+               "P2_S22"
+            ],
+            "id": "S9_S22",
+            "noSemaine": 9
+         },
+         {
+            "idProgrammes": [
+               "P0_S22",
+               "P1_S22",
+               "P2_S22"
+            ],
+            "id": "S10_S22",
+            "noSemaine": 10
+         },
+         {
+            "idProgrammes": [
+               "P0_S22",
+               "P1_S22",
+               "P2_S22"
+            ],
+            "id": "S11_S22",
+            "noSemaine": 11
+         },
+         {
+            "idProgrammes": [
+               "P0_S22",
+               "P1_S22",
+               "P2_S22"
+            ],
+            "id": "S12_S22",
+            "noSemaine": 12
+         },
+         {
+            "idProgrammes": [
+               "P0_S22",
+               "P1_S22",
+               "P2_S22"
+            ],
+            "id": "S13_S22",
+            "noSemaine": 13
+         },
+         {
+            "idProgrammes": [
+               "P0_S22",
+               "P1_S22",
+               "P2_S22"
+            ],
+            "id": "S14_S22",
+            "noSemaine": 14
+         },
+         {
+            "idProgrammes": [
+               "P0_S22",
+               "P1_S22",
+               "P2_S22"
+            ],
+            "id": "S15_S22",
+            "noSemaine": 15
+         }
+      ],
+      "id": "S22",
+      "nom": "Session 2022",
+      "description": "Session plein de plaisir",
+      "dateDebut": "2022-04-01",
+      "dateFin": "2022-08-01"
+   },
+   {
+      "semaines": [
+         {
+            "idProgrammes": [
+               "P0_S20",
+               "P1_S20",
+               "P2_S20"
+            ],
+            "id": "S1_S20",
+            "noSemaine": 1
+         },
+         {
+            "idProgrammes": [
+               "P0_S20",
+               "P1_S20",
+               "P2_S20"
+            ],
+            "id": "S2_S20",
+            "noSemaine": 2
+         },
+         {
+            "idProgrammes": [
+               "P0_S20",
+               "P1_S20",
+               "P2_S20"
+            ],
+            "id": "S3_S20",
+            "noSemaine": 3
+         },
+         {
+            "idProgrammes": [
+               "P0_S20",
+               "P1_S20",
+               "P2_S20"
+            ],
+            "id": "S4_S20",
+            "noSemaine": 4
+         },
+         {
+            "idProgrammes": [
+               "P0_S20",
+               "P1_S20",
+               "P2_S20"
+            ],
+            "id": "S5_S20",
+            "noSemaine": 5
+         },
+         {
+            "idProgrammes": [
+               "P0_S20",
+               "P1_S20",
+               "P2_S20"
+            ],
+            "id": "S6_S20",
+            "noSemaine": 6
+         },
+         {
+            "idProgrammes": [
+               "P0_S20",
+               "P1_S20",
+               "P2_S20"
+            ],
+            "id": "S7_S20",
+            "noSemaine": 7
+         },
+         {
+            "idProgrammes": [
+               "P0_S20",
+               "P1_S20",
+               "P2_S20"
+            ],
+            "id": "S8_S20",
+            "noSemaine": 8
+         },
+         {
+            "idProgrammes": [
+               "P0_S20",
+               "P1_S20",
+               "P2_S20"
+            ],
+            "id": "S9_S20",
+            "noSemaine": 9
+         },
+         {
+            "idProgrammes": [
+               "P0_S20",
+               "P1_S20",
+               "P2_S20"
+            ],
+            "id": "S10_S20",
+            "noSemaine": 10
+         },
+         {
+            "idProgrammes": [
+               "P0_S20",
+               "P1_S20",
+               "P2_S20"
+            ],
+            "id": "S11_S20",
+            "noSemaine": 11
+         },
+         {
+            "idProgrammes": [
+               "P0_S20",
+               "P1_S20",
+               "P2_S20"
+            ],
+            "id": "S12_S20",
+            "noSemaine": 12
+         },
+         {
+            "idProgrammes": [
+               "P0_S20",
+               "P1_S20",
+               "P2_S20"
+            ],
+            "id": "S13_S20",
+            "noSemaine": 13
+         },
+         {
+            "idProgrammes": [
+               "P0_S20",
+               "P1_S20",
+               "P2_S20"
+            ],
+            "id": "S14_S20",
+            "noSemaine": 14
+         },
+         {
+            "idProgrammes": [
+               "P0_S20",
+               "P1_S20",
+               "P2_S20"
+            ],
+            "id": "S15_S20",
+            "noSemaine": 15
+         }
+      ],
+      "id": "S20",
+      "nom": "Session 2020",
+      "description": "Session plein de plaisir",
+      "dateDebut": "2020-04-01",
+      "dateFin": "2020-08-01"
+   }
+]
